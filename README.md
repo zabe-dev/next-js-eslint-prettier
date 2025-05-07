8ee89fbb9ca4cff6b403eb259d9a308f236cdaf5
zabe <jay@zabe.dev>
2025-05-07 13:37:21 +0800

    chore: set up init configuration

---
+++ README.md
@@ -0,0 +1,1 @@
+Next.js w/ ESLint + Prettier configuration
---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+	return (
+		<main className="flex h-screen w-full items-center justify-center">
+			<h2 className="text-md font-semibold">Next.js + ESLint + Prettier</h2>
+		</main>
+	);
+}
